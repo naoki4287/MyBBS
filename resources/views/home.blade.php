cf4bd989cc6bd2cf3ce5fae20ab6907faf8c0bd8
github naoki4287 <github@waka.naoki86@icloud.com>
2022-06-21 17:21:23 +0900

    ✨ ペジネーション機能を実装しました

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,19 @@
     <div class="">
       @foreach ($comments as $comment)
       <div class="mt-2 w-full">
-        <span class="ml-4">{{ $comment['name'] }}</span><span class="ml-4 float-right">{{ $comment['created_at']}}</span>
-        <div class="border-b-2 border-gray-700 mt-1 ml-4">{{ $comment['comment']}}</div>
+        <span class="ml-4">{{ $comment->name }}</span><span class="ml-4 float-right">{{ $comment->created_at }}</span>
+        <div class="border-b-2 border-gray-700 mt-1 ml-4">{{ $comment->comment }}</div>
       </div>
       @endforeach
+      <div class="mt-12 flex justify-center">
+        {{ $comments->appends(['sort' => $sort])->links('tailwind') }}
+      </div>
     </div>
     
     <div id="modal" class="modal hidden w-full h-full bg-gray-800 z-10 text-center">
       <form action="post" method="POST">
         @csrf
-        <input type="text" name="name" placeholder="名前" autofocus autocomplete="off" class="w-8/12 mt-8 mb-4 bg-gray-700 rounded-md text-white outline-none"><br>
+        <input type="text" name="name" placeholder="名前" autofocus  autocomplete="off" class="w-8/12 mt-8 mb-4 bg-gray-700 rounded-md text-white outline-none"><br>
         <textarea name="comment" cols="" rows="10" placeholder="いま暇な人いる？" class="w-8/12 my-4 bg-gray-700 text-white rounded-md outline-none"></textarea>
         <div class="space-x-48">
           <button type="button" id="cancelBtn" class="my-4 p-2 text-white rounded-md  hover:bg-gray-600">キャンセル</button>
@@ -22,11 +25,11 @@
       </form>
     </div>
     <div id="mask" class="hidden fixed inset-0 z-0"></div>
-
+    
     <div class="flex justify-end">
-      <i id="postBtn" class="fas fa-4x fa-plus-circle  text-gray-700 "></i>
+      <i id="postBtn" class="fas fa-4x fa-plus-circle text-gray-700  z-10"></i>
     </div>
-
+    
   </div>
   <script type="module" src="{{ asset('js/home.js') }}"></script>
 </x-app-layout>
