81396e7ab838693a3eeb4b9cc1270047363f41b9
github naoki4287 <github@waka.naoki86@icloud.com>
2022-06-24 19:20:46 +0900

    🩹

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -8,8 +8,8 @@
       @error('comment')
       <div class="text-white text-center mt-6 -mb-6">{{ $message }}</div>
       @enderror
-      <x-input name="name" id="name" autocomplete="off"></x-input><br>
-      <x-textarea  name="comment" placeholder="今暇な人いる？"></x-textarea>
+      <x-input name="name" id="name" autofocus autocomplete="off"></x-input><br>
+      <x-textarea name="comment" placeholder="今暇な人いる？"></x-textarea>
       <div class="space-x-48">
         <x-a_back class="text-white">キャンセル</x-a_back>
         <x-button class="bg-red-700 hover:bg-red-600">投稿する</x-button>
